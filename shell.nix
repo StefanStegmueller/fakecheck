45b904cb2872b89e313d6cec4a551dc6f6cc25a9
Stefan <Stefan.Steg@gmail.com>
2020-11-15 17:05:54 +0100

    added simple model training to checkthat_hansen notebook

--- shell.nix
+++ shell.nix
@@ -16,6 +16,7 @@ let
                             tensorflow
                             tensorflow-tensorboard
                             Keras
+                            scikitlearn
                           ];
   };
 
