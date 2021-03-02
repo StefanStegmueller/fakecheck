b8f359dbe98aa2b664bdd02804386997d413fe8a
Stefan <Stefan.Steg@gmail.com>
2021-03-02 11:55:42 +0100

    enabled tensorboard and addded metrics to bert notebook

--- notebooks/jupyter_config/start-docker.sh
+++ notebooks/jupyter_config/start-docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -p 8888:8888 -e JUPYTER_ENABLE_LAB=yes -v $1:/home/jovyan/work jupyter/fakecheck start-notebook.sh --NotebookApp.password='sha1:5a0e54ce83e1:9fc3c47be9ec303e4a14a7a5a4c01dbdeff6a16e' 
+docker run -p 8888:8888 -p 6006:6006 -e JUPYTER_ENABLE_LAB=yes -v $1:/home/jovyan/work jupyter/fakecheck start-notebook.sh --NotebookApp.password='sha1:5a0e54ce83e1:9fc3c47be9ec303e4a14a7a5a4c01dbdeff6a16e' 
